907c5d8790ade9f53f34991c0d9ea0912a11640f
koushith <koushith97@gmail.com>
2021-11-16 01:20:33 +0530

    adds the starter files

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,47 @@
+import { Pane, majorScale, Button, Text, Badge, SelectMenu } from 'evergreen-ui';
 import React from 'react';
 
 import './App.css';
 
 function App() {
   return (
-    <div className='App'>
-      <h1>Welcome to Problems Hunt</h1>
-    </div>
+    <>
+      <Pane>
+        <Badge color='neutral' marginRight={8}>
+          neutral
+        </Badge>
+        <Badge color='green' marginRight={8}>
+          green
+        </Badge>
+        <Badge color='blue' marginRight={8}>
+          blue
+        </Badge>
+        <Badge color='red' marginRight={8}>
+          red
+        </Badge>
+        <Badge color='orange' marginRight={8}>
+          orange
+        </Badge>
+        <Badge color='purple' marginRight={8}>
+          purple
+        </Badge>
+        <Badge color='yellow' marginRight={8}>
+          yellow
+        </Badge>
+        <Badge color='teal' marginRight={8}>
+          teal
+        </Badge>
+
+        <SelectMenu
+          title='Select name'
+          options={['Apple', 'Apricot', 'Banana', 'Cherry', 'Cucumber'].map((label) => ({ label, value: label }))}
+          selected={selected}
+          onSelect={(item) => setSelected(item.value)}
+        >
+          <Button>{selected || 'Select name...'}</Button>
+        </SelectMenu>
+      </Pane>
+    </>
   );
 }
 
