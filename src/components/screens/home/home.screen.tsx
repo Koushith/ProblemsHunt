1e62d398171cdd2c14a2c08506c7dc4efd34fb1a
koushith <koushith97@gmail.com>
2022-01-08 00:56:49 +0530

    renames to generic-problem-statement

--- src/components/screens/home/home.screen.tsx
+++ src/components/screens/home/home.screen.tsx
@@ -1,7 +1,7 @@
 import { Pane } from 'evergreen-ui';
 import { NavBar } from '../../../components';
-import { GenericProblemStatementCard } from '../../generic-problem-statement-card/generic-problem-statement-card.component';
 import { SideBar } from '../../side-bar/sidebar-component';
+
 export const HomeScreen = () => {
   return (
     <>
